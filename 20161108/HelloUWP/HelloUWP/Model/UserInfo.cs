75907fa0b723739fc1db63284b35e0f285751771
Minegi <zhaoweic18@gmail.com>
2016-11-08 00:43:03 +0900

    UWP에서 LINQ의 Dynamic Query 사용하기 소스 업로드

---
+++ 20161108/HelloUWP/HelloUWP/Model/UserInfo.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloUWP.Model
+{
+    public class UserInfo
+    {
+        [JsonProperty(PropertyName = "id")]
+        public int Id
+        {
+            get; set;
+        }
+
+        [JsonProperty(PropertyName = "first_name")]
+        public string FirstName
+        {
+            get; set;
+        }
+
+        [JsonProperty(PropertyName = "last_name")]
+        public string LastName
+        {
+            get; set;
+        }
+
+        [JsonProperty(PropertyName = "email")]
+        public string Email
+        {
+            get; set;
+        }
+
+        [JsonProperty(PropertyName = "gender")]
+        public string Gender
+        {
+            get; set;
+        }
+
+        [JsonProperty(PropertyName = "ip_address")]
+        public string IpAddress
+        {
+            get; set;
+        }
+    }
+
+    public class UserInfoList
+    {
+        public List<UserInfo> records { get; set; }
+    }
+}
---
+++ 20161108/HelloUWP/HelloUnitTest/UnitTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using Windows.Storage;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using HelloUWP.Model;
+using System.Linq;
+using System.Linq.Dynamic.Core;
+
+namespace HelloUnitTest
+{
+    [TestClass]
+    public class JsonTest
+    {
+        private UserInfoList data_;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            Task<StorageFolder> getAssetFolderTask = Windows.ApplicationModel.Package.Current.InstalledLocation.GetFolderAsync("Assets").AsTask();
+            getAssetFolderTask.Wait();
+
+            StorageFolder assetFolder = getAssetFolderTask.Result;
+
+            Task<StorageFile> getJsonFileTask = assetFolder.GetFileAsync("MOCK_DATA.json").AsTask();
+            getJsonFileTask.Wait();
+
+            StorageFile jsonFile = getJsonFileTask.Result;
+
+            Task<string> getJsonStringTask = FileIO.ReadTextAsync(jsonFile).AsTask();
+            getJsonStringTask.Wait();
+
+            string jsonString = getJsonStringTask.Result;
+
+            data_ = JsonConvert.DeserializeObject<UserInfoList>(jsonString);
+        }
+
+        [TestMethod]
+        public void DynamicLinqTest()
+        {
+            var query = data_.records.AsQueryable();
+            var condition = "Id=1";
+            //"id":1,"first_name":"Nancy","last_name":"Kelley","email":"nkelley0@mapy.cz","gender":"Female","ip_address":"145.64.248.83"
+            var result = query.Where(condition);
+            UserInfo user = result.First();
+
+            Assert.AreEqual(result.Count(), 1);
+            Assert.AreEqual(user.FirstName, "Nancy");
+            Assert.AreEqual(user.LastName, "Kelley");
+            Assert.AreEqual(user.Email, "nkelley0@mapy.cz");
+            Assert.AreEqual(user.Gender, "Female");
+            Assert.AreEqual(user.IpAddress, "145.64.248.83");
+        }
+    }
+}
